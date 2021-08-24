3ed2ae5260bb85245a9c68d53373788904e2a397
Eric <eric868.ji@gmail.com>
2021-08-23 21:45:46 -0400

    created app and started turning class based components into functional components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
+  const [user, setUser] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
