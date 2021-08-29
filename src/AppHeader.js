5855d993b4a85c3152ef35b20c8db275a88994e2
Eric <eric868.ji@gmail.com>
2021-08-29 12:09:43 -0400

    updated images, logos, and website title

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import logo from './logo.svg';
+import rpslogo from './rpslogo.svg';
 import './AppHeader.css';
 
 function AppHeader() {
     return (
         <div className="AppHeader">
             <p style={{margin: "0.5em"}} className="AppHeader-text">
-            <img src={logo} className="App-logo" alt="logo" />
+            <img src={rpslogo} className="App-logo" alt="logo" />
             Rock Paper Scissors
             </p>
         </div>
